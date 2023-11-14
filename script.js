9624ee963be2216a6454b4e9403bc9c8b420f29a
John Doe <johndoe@example.com>
2023-11-14 10:08:10 +0100

    lagt til farger

--- script.js
+++ script.js
@@ -1,10 +1,56 @@
 
 const fileInputEl = document.querySelector("#fileInput")
+const barContainerEl = document.querySelector("#barContainer")
+
+const colors = {}
+
+function makeBar(info) {
+
+    const barEl = document.createElement("div")
+
+    barEl.className = "bar"
+
+    const infoArr = Object.entries(info)
+    const total = Object.values(info).reduce((n, i) => Number(n) + Number(i))
+
+
+
+
+    infoArr.forEach(typeAndNum => {
+        percent = Math.floor((Number(typeAndNum[1]) / total) * 100)
+        
+        const barPartEl = document.createElement("div")
+        
+        barPartEl.className = "barPart"
+        barPartEl.style.width = `${percent}%`
+        barPartEl.style.backgroundColor = colors[typeAndNum[0]]
+
+        barEl.appendChild(barPartEl)
+
+    });
+
+    return barEl
+}
 
 function makeBars(data) {
 
     console.log(data);
 
+    for (const year in data) {
+        const divEl = document.createElement("div")
+
+        divEl.innerHTML += `<h1>${year}</h1>`
+
+        for (const area in data[year]) {
+            
+            const barEl = makeBar(data[year][area])
+
+            divEl.appendChild(barEl)
+
+        }
+
+        barContainerEl.appendChild(divEl)
+    }
 }
 
 function makeData(table) {
@@ -20,6 +66,12 @@ function makeData(table) {
     let carTypeCount = 0
 
     for (let i = 1; i < table.length; i++) {
+
+        const red = Math.floor(Math.random() * 200)
+        const green = Math.floor(Math.random() * 200)
+        const blue = Math.floor(Math.random() * 200)
+
+        colors[table[i][1]] = `#${(red << 16 | green << 8 | blue).toString(16).padStart(6, '0')}`
         if (table[i][0]) {
             carTypeCount = i
             break
